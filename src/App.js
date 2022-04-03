6c8133b71e6555b9a3b3e235fb672719d706a9f6
rirefat <rirefat.official@gmail.com>
2022-04-04 00:06:18 +0600

    Done Navbar Part

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import './App.css';
 import banner from '../src/components/Images/top-banner.jpg'
 import TopBanner from './components/TopBanner/TopBanner';
+import Header from './components/Header/Header';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div>
       {/* <img src={banner} alt="" /> */}
-      <TopBanner></TopBanner>
+      <Header></Header>
+      {/* <TopBanner></TopBanner> */}
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/reviews' element={<Home></Home>}></Route>
+        <Route path='/dashboard' element={<Home></Home>}></Route>
+        <Route path='/blogs' element={<Home></Home>}></Route>
+        <Route path='/About' element={<Home></Home>}></Route>
+      </Routes>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../Images/logo-white.png'
 import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            
+            <nav className='nav-bar'>
+                <div className="nav-icon">
+                    <img src={logo} alt="" />
+                </div>
+                <div className="nav-menu">
+                    <Link to='/'>Home</Link>
+                    <Link to='/reviews'>Reviews</Link>
+                    <Link to='/dashboard'>Dashboard</Link>
+                    <Link to='/blogs'>Blogs</Link>
+                    <Link to='/about'>About</Link>
+                </div>
+            </nav>
         </div>
     );
 };
