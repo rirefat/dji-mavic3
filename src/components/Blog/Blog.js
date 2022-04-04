38906cc07d53047d0aad4ee5f2dfc548c10ac2ba
rirefat <rirefat.official@gmail.com>
2022-04-04 23:57:43 +0600

    Edit blog page

--- src/components/Blog/Blog.js
+++ src/components/Blog/Blog.js
@@ -7,6 +7,20 @@ const Blog = () => {
             <div className="title">
                 <h1><span className='target-text'>Blog</span> Page</h1>
             </div>
+            <div className="qna-container">
+                <div className="single-qna">
+                    <h3><span># Q1. </span>What is the purpose of Context API?</h3>
+                    <p><strong>Ans:</strong> </p>
+                </div>
+                <div className="single-qna">
+                    <h3><span># Q2. </span>What is the symantic tag?</h3>
+                    <p><strong>Ans:</strong> </p>
+                </div>
+                <div className="single-qna">
+                    <h3><span># Q3. </span>What are the differences between inline, block and inline-block elements?</h3>
+                    <p><strong>Ans:</strong> </p>
+                </div>
+            </div>
         </div>
     );
 };
